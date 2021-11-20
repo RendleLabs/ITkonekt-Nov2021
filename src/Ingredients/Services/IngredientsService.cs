4345f5f8ce984612eb0d6b0139edd8d10199f47e
Mark <mark@rendlelabs.com>
2021-11-20 10:14:28 +0000

    Add Ingredients service

---
+++ src/Ingredients/Services/IngredientsService.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Core;
+using Ingredients.Data;
+using Ingredients.Protos;
+
+namespace Ingredients.Services;
+
+public class IngredientsService : Protos.IngredientsService.IngredientsServiceBase
+{
+    private readonly IToppingData _toppingData;
+    private readonly ILogger<IngredientsService> _logger;
+
+    public IngredientsService(IToppingData toppingData, ILogger<IngredientsService> logger)
+    {
+        _toppingData = toppingData;
+        _logger = logger;
+    }
+
+    public override async Task<GetToppingsResponse> GetToppings(GetToppingsRequest request, ServerCallContext context)
+    {
+        _logger.LogInformation("Getting toppings");
+
+        try
+        {
+            var toppings = await _toppingData.GetAsync(context.CancellationToken);
+            var response = new GetToppingsResponse
+            {
+                Toppings =
+                {
+                    toppings.Select(t => new Topping
+                    {
+                        Id = t.Id,
+                        Name = t.Name,
+                        Price = t.Price
+                    })
+                }
+            };
+
+            return response;
+        }
+        catch (OperationCanceledException)
+        {
+            _logger.LogWarning("Operation was cancelled");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error: {ex.Message}");
+            throw;
+        }
+    }
+}
